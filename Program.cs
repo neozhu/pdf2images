53f664c2c0902a92cd0fbc5d93ae655787db083b
neozhu <new163@163.com>
2025-07-01 08:28:00 +0800

    Add Serilog file logging and Windows Service configuration

--- Program.cs
+++ Program.cs
@@ -4,12 +4,15 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 using pdf2images;
+using Serilog;
+using Microsoft.Extensions.Configuration;
 
 // Configure unhandled exception handler for the entire application
 AppDomain.CurrentDomain.UnhandledException += (sender, args) => 
 {
     var exception = args.ExceptionObject as Exception;
-    HandleGlobalException(exception, "Unhandled Exception");
+    if (exception != null)
+        HandleGlobalException(exception, "Unhandled Exception");
 };
 
 // Configure tasks to observe unhandled exceptions
@@ -92,7 +95,7 @@ static async Task SendExceptionEmailAsync(Exception ex, string type)
     }
 }
 
-static string GetInnerExceptionHtml(Exception innerEx)
+static string GetInnerExceptionHtml(Exception? innerEx)
 {
     if (innerEx == null)
     {
@@ -120,7 +123,30 @@ static string GetInnerExceptionHtml(Exception innerEx)
 // Main program
 try
 {
+    // Configure Serilog
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .WriteTo.Console()
+        .WriteTo.File(
+            path: "logs/pdf2images-.txt",
+            rollingInterval: RollingInterval.Day,
+            rollOnFileSizeLimit: true,
+            fileSizeLimitBytes: 10485760,
+            retainedFileCountLimit: 10,
+            buffered: false,
+            flushToDiskInterval: TimeSpan.FromSeconds(1))
+        .CreateLogger();
+
     var builder = Host.CreateApplicationBuilder(args);
+    
+    // Use Serilog as the logging provider
+    builder.Services.AddSerilog();
+    
+    // Configure Windows Service support
+    builder.Services.AddWindowsService(options =>
+    {
+        options.ServiceName = "PDF2Images Service";
+    });
     
     // Register services for dependency injection
     builder.Services.AddSingleton<EmailService>(provider => 
@@ -148,10 +174,15 @@ try
 catch (Exception ex)
 {
     // Handle startup exceptions
+    Log.Fatal(ex, "Application startup failed");
     await SendExceptionEmailAsync(ex, "Application Startup Error");
     
     // Re-throw to terminate the application if it can't start properly
     throw;
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
 
